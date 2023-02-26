9862a9119eb5250b9833ed4580da9cde13df466c
Daneos08 <113821056+Daneos08@users.noreply.github.com>
2023-02-26 08:47:10 +0000

    add Class Student, Teacher

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 class Persosn:
-  name = "Name"
+  def __init__(self, name):
+   self.name = name
 
-def __init__(self, name):
-  self.name = name
+class Studend(Person):
+  grade = 0
+  def study(self):
+    self.grade += 1
+
+  def __init__(self):
+    super().__init("student")
+
+class Teacher(Person):
+  salary = 0
+  student = []
+
+student = Student
+  
   
