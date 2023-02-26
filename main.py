1866c6e974c0ea1b369c8684d36c6ef62603767e
Daneos08 <113821056+Daneos08@users.noreply.github.com>
2023-02-26 09:20:59 +0000

    add worck teacher

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ class Person:
   def __init__(self, name):
    self.name = name
 
+
 class Student(Person):
   grade = 0
+  
   def study(self):
-    self.grade += 1
+    self.grade += 10
 
   def __init__(self):
     super().__init__("Student")
+
 
 class Teacher(Person):
   salary = 0
   student = []
 
-student = Student()
+  def worck(self):
+    self.salary += 10000
+
+  def __init__(self):
+    super().__init__("Teacher")
   
+    
+
+
+student = Student()
+print(student.grade)
+student.study()
 print(student.grade)
 
-student.study()
-student.study()
-student.study()
 
-print(student.grade)
+teacher = Teacher()
+teacher.worck()
+print(teacher.salary)
